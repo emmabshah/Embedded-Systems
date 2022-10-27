3ca478147a49c6d8a95b47f8a06fb3091b2953b4
emmabshah <emma.shah@students.plymouth.ac.uk>
2022-10-27 12:57:32 +0100

    ew

--- Tasks/Task-126-DigitalIn/main.cpp
+++ Tasks/Task-126-DigitalIn/main.cpp
@@ -1,12 +1,19 @@
 #include "mbed.h"
 
 DigitalIn ButtonA(PG_0); //Button A
+DigitalIn ButtonB(PG_1);
+DigitalIn ButtonC(PG_2, PinMode :: PullDown);
+DigitalIn ButtonD(PG_3, PinMode :: PullDown);
 DigitalOut redLED(PC_2); //Red Traffic 1
 
 // main() runs in its own thread in the OS
 int main()
 {
     int btnA;
+    int btnB;
+    int btnC;
+    int btnD; 
+    
     // Turn OFF the red LED
     redLED = 0;
 
@@ -15,11 +22,15 @@ int main()
         // Wait for the button to be pressed
         do {
             btnA = ButtonA; //Read button A
-        } while (btnA == 0);
+            btnB = ButtonB; 
+            btnC = ButtonC;
+            btnD = ButtonD;
+        } while ((btnA && btnB && btnC && btnD ) ==0);
 
 
         //Toggle the red LED
         redLED = !redLED;
+       
 
         //Wait for noise to settle
         wait_us(10000);
@@ -27,10 +38,14 @@ int main()
         // Wait for the button to be released
         do {
             btnA = ButtonA; //Read button A
-        } while (btnA == 1);
+            btnB = ButtonB; 
+            btnC = ButtonC;
+            btnD = ButtonD;
+        } while ((btnA && btnB && btnC && btnD) == 1);
 
         //Wait for noise to settle
         wait_us(10000);
     }
+
 }
 
